4dab98695d7251ae61bd224c18609efe9b6daf44
Piyush Jain <piyushjain@duck.com>
2022-09-09 20:46:45 -0700

    Imported server api, default UI extension from cookiecutter

---
+++ jupyter_scheduler/__init__.py
@@ -0,0 +1,27 @@
+"""Scheduling API for JupyterLab"""
+import json
+from pathlib import Path
+
+from .extension import SchedulerApp
+from ._version import __version__
+
+
+HERE = Path(__file__).parent.resolve()
+
+
+with (HERE / "labextension" / "package.json").open() as fid:
+    data = json.load(fid)
+
+
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": data["name"]
+    }]
+
+
+def _jupyter_server_extension_points():
+    return [{
+        "module": "jupyter_scheduler",
+        "app": SchedulerApp
+    }]
