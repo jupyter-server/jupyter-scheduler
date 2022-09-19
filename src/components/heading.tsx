ff23c6cc5efb0d632a2537e4452b85b234e80e11
Brian E. Granger <brgrange@amazon.com>
2022-09-19 11:00:49 -0700

    Update CSS classes to avoid global conficts.

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -8,12 +8,12 @@ export interface IHeadingProps {
 export function Heading(props: IHeadingProps): JSX.Element {
   switch (props.level) {
     case 1:
-      return <h1 className="jp-Heading">{props.children}</h1>;
+      return <h1 className="jp-jobs-Heading">{props.children}</h1>;
     case 2:
-      return <h2 className="jp-Heading">{props.children}</h2>;
+      return <h2 className="jp-jobs-Heading">{props.children}</h2>;
     case 3:
-      return <h3 className="jp-Heading">{props.children}</h3>;
+      return <h3 className="jp-jobs-Heading">{props.children}</h3>;
     default:
-      return <h1 className="jp-Heading">{props.children}</h1>;
+      return <h1 className="jp-jobs-Heading">{props.children}</h1>;
   }
 }
