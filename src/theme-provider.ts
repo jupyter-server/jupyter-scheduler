acd35a48044789bc4a6efa28da6a645f95928387
Brian E. Granger <brgrange@amazon.com>
2022-09-19 19:48:18 -0700

    Update theme provider.

--- src/theme-provider.ts
+++ src/theme-provider.ts
@@ -2,12 +2,17 @@ import { Theme, createTheme } from '@mui/material/styles';
 
 export function getJupyterLabTheme(): Theme {
   return createTheme({
-    spacing: 2,
+    spacing: 4,
     palette: {
       primary: {
         main: getComputedStyle(document.body).getPropertyValue(
           '--jp-brand-color1'
         )
+      }
+    },
+    typography: {
+      button: {
+        textTransform: 'capitalize'
       }
     }
   });
