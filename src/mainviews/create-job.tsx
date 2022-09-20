b6b2fbbea1d5123149bc103078f593fc10cb470f
Brian E. Granger <brgrange@amazon.com>
2022-09-19 21:45:17 -0700

    More explorations with MUI.

--- src/mainviews/create-job.tsx
+++ src/mainviews/create-job.tsx
@@ -268,7 +268,6 @@ export function CreateJob(props: ICreateJobProps): JSX.Element {
           </Cluster>
         </Stack>
       </form>
-      <TextField id="outlined-basic" size="small" label="Outlined" variant="outlined" />
     </Box>
   );
 }
