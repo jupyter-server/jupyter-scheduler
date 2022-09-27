55b30c3984eabde3677020c275bb90d8f9d93928
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-09-27 05:14:07 +0000

    Publish 0.2.1
    SHA256 hashes:
    
    jupyter_scheduler-0.2.1-py3-none-any.whl: e205df07a23fef368b65fcec8486020c9cb71e1700464f75014159bd225b90f7
    
    jupyter_scheduler-0.2.1.tar.gz: 5ef7f2271011ecf8da7a7a6f4c71d09d07639314da0b01ccbf2c72086d59881d
    
    jupyterlab-scheduler-0.2.1.tgz: bac4f3e4ec5ae6ebff5086ffc271ce26067ca908e64cc52c6cc487faf69f7f64

--- jupyter_scheduler/_version.py
+++ jupyter_scheduler/_version.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 __all__ = ["__version__"]
 
-version_info = (0, 1, 1, "", "")
+version_info = (0, 2, 1, "", "")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
