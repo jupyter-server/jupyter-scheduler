1fcc10804f2295f545b15518252cc93cd3fd781a
Brian E. Granger <ellisonbg@gmail.com>
2022-09-30 14:51:50 -0700

    Theme improvements to better align with JupyterLab styling (#61)
    * Improvements to the MUI theme for JupyterLab.
    
    * Additional theme improvements.
    
    Co-authored-by: Brian E. Granger <brgrange@amazon.com>

--- src/theme-provider.ts
+++ src/theme-provider.ts
@@ -8,16 +8,105 @@ export function getJupyterLabTheme(): Theme {
   const light = document.body.getAttribute('data-jp-theme-light');
   return createTheme({
     spacing: 4,
+    components: {
+      MuiButton: {
+        defaultProps: {
+          size: 'small'
+        }
+      },
+      MuiFilledInput: {
+        defaultProps: {
+          margin: 'dense'
+        }
+      },
+      MuiFormControl: {
+        defaultProps: {
+          margin: 'dense'
+        }
+      },
+      MuiFormHelperText: {
+        defaultProps: {
+          margin: 'dense'
+        }
+      },
+      MuiIconButton: {
+        defaultProps: {
+          size: 'small'
+        }
+      },
+      MuiInputBase: {
+        defaultProps: {
+          margin: 'dense',
+          size: 'small'
+        }
+      },
+      MuiInputLabel: {
+        defaultProps: {
+          margin: 'dense'
+        }
+      },
+      MuiListItem: {
+        defaultProps: {
+          dense: true
+        }
+      },
+      MuiOutlinedInput: {
+        defaultProps: {
+          margin: 'dense'
+        }
+      },
+      MuiFab: {
+        defaultProps: {
+          size: 'small'
+        }
+      },
+      MuiTable: {
+        defaultProps: {
+          size: 'small'
+        }
+      },
+      MuiTextField: {
+        defaultProps: {
+          margin: 'dense',
+          size: 'small'
+        }
+      },
+      MuiToolbar: {
+        defaultProps: {
+          variant: 'dense'
+        }
+      }
+    },
     palette: {
       mode: light === 'true' ? 'light' : 'dark',
       primary: {
-        main: getCSSVariable('--jp-brand-color1')
+        main: getCSSVariable('--jp-brand-color1'),
+        light: getCSSVariable('--jp-brand-color2'),
+        dark: getCSSVariable('--jp-brand-color0')
+      },
+      error: {
+        main: getCSSVariable('--jp-error-color1'),
+        light: getCSSVariable('--jp-error-color2'),
+        dark: getCSSVariable('--jp-error-color0')
+      },
+      warning: {
+        main: getCSSVariable('--jp-warn-color1'),
+        light: getCSSVariable('--jp-warn-color2'),
+        dark: getCSSVariable('--jp-warn-color0')
+      },
+      success: {
+        main: getCSSVariable('--jp-success-color1'),
+        light: getCSSVariable('--jp-success-color2'),
+        dark: getCSSVariable('--jp-success-color0')
       },
       text: {
         primary: getCSSVariable('--jp-ui-font-color1'),
         secondary: getCSSVariable('--jp-ui-font-color2'),
         disabled: getCSSVariable('--jp-ui-font-color3')
       }
+    },
+    shape: {
+      borderRadius: 2
     },
     typography: {
       fontFamily: getCSSVariable('--jp-ui-font-family'),
