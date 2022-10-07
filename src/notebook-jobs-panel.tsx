037c8b588ceb7add238154ca368c0e623b8ed4f0
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2022-10-07 12:40:11 -0700

    add index for detail-view folder to import from, update imports

--- src/notebook-jobs-panel.tsx
+++ src/notebook-jobs-panel.tsx
@@ -14,7 +14,7 @@ import { NotebookJobsList } from './mainviews/list-jobs';
 import { ICreateJobModel, JobsModel } from './model';
 import { getJupyterLabTheme } from './theme-provider';
 import { Scheduler } from './tokens';
-import { DetailView } from './mainviews/detail-view/detail-view';
+import { DetailView } from './mainviews/detail-view/index';
 
 export class NotebookJobsPanel extends VDomRenderer<JobsModel> {
   readonly _title?: string;
