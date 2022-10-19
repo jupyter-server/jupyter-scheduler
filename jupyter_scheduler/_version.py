f8c077ca1cef6634711cfdf90ceadb360b283e22
Piyush Jain <piyushjain@duck.com>
2022-10-19 13:41:40 -0700

    Bump to 0.5.1

--- jupyter_scheduler/_version.py
+++ jupyter_scheduler/_version.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 __all__ = ["__version__"]
 
-version_info = (0, 5, 0, "", "")
+version_info = (0, 5, 1, "", "")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
