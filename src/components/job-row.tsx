e3dfc65908a4dc58ce189611347ed57cb2f07c45
David L. Qiu <david@qiu.dev>
2022-11-02 23:19:53 +0000

    merge import declarations

--- src/components/job-row.tsx
+++ src/components/job-row.tsx
@@ -9,8 +9,7 @@ import { useTranslator } from '../hooks';
 import { ICreateJobModel } from '../model';
 import DownloadIcon from '@mui/icons-material/Download';
 import StopIcon from '@mui/icons-material/Stop';
-import { Link } from '@mui/material';
-import { IconButton, Stack, TableCell, TableRow } from '@mui/material';
+import { IconButton, Stack, Link, TableCell, TableRow } from '@mui/material';
 
 function StopButton(props: {
   job: Scheduler.IDescribeJob;
