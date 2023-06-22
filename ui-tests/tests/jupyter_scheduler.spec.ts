e69d809f8939adf9394ab1e9248c05bc6820ee78
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-06-22 10:58:27 -0700

    add launcher UI test

--- ui-tests/tests/jupyter_scheduler.spec.ts
+++ ui-tests/tests/jupyter_scheduler.spec.ts
@@ -1,11 +1,8 @@
 import { expect, test } from '@jupyterlab/galata';
 
-/**
- * Don't load JupyterLab webpage before running the tests.
- * This is required to ensure we capture all log messages.
- */
-test.use({ autoGoto: false });
-
-test('should emit an activation console message', async ({ page }) => {
-  // TODO: Add real tests
+test('Should add a Notebook Jobs card to JupyterLab launcher', async ({
+  page
+}) => {
+  await page.getByRole('tabpanel', { name: 'Launcher' }).waitFor({ state: 'detached' });
+  expect(page.getByTitle('Notebook Jobs')).toBeDefined(); 
 });
