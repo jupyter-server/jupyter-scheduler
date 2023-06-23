f860498c7974fcf4777bad6bda8901d13baeb9f5
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-06-22 23:40:39 -0700

    add back autoGoto: false

--- ui-tests/tests/jupyter_scheduler.spec.ts
+++ ui-tests/tests/jupyter_scheduler.spec.ts
@@ -1,8 +1,14 @@
 import { expect, test } from '@jupyterlab/galata';
 
+/**
+ * Don't load JupyterLab webpage before running the tests.
+ * This is required to ensure we capture all log messages.
+ */
+test.use({ autoGoto: false });
+
 test('Should add a Notebook Jobs card to JupyterLab launcher', async ({
   page
 }) => {
-  await page.getByRole('tabpanel', { name: 'Launcher' });
+  page.getByRole('tabpanel', { name: 'Launcher' });
   expect(page.getByTitle('Notebook Job')).toBeDefined();
 });
