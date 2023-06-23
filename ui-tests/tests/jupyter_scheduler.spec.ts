92ddff2cfa44e1af01d1fae38150abd7be1b1f2c
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-06-23 14:52:57 -0700

    test visibility of create a job btn in notebook toolbar

--- ui-tests/tests/jupyter_scheduler.spec.ts
+++ ui-tests/tests/jupyter_scheduler.spec.ts
@@ -16,3 +16,13 @@ test('Notebook Jobs card is visible in JupyterLab launcher', async ({
 
   await expect(jobsEl).toBeVisible();
 });
+
+test('"Create a notebook job" button is visible in notebook toolbar', async ({
+  page
+}) => {
+  await page.goto();
+  await page.notebook.createNew();
+  const createJobButton = page.locator('button.jp-ToolbarButtonComponent[data-command="scheduling:create-from-notebook"][title="Create a notebook job"]');
+
+  await expect(createJobButton).toBeVisible();
+});
