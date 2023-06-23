d535889d3bec992b7a30af8357c2aa26bac153bd
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-06-22 23:16:20 -0700

    stop waiting for launcher to be detached

--- ui-tests/tests/jupyter_scheduler.spec.ts
+++ ui-tests/tests/jupyter_scheduler.spec.ts
@@ -3,6 +3,6 @@ import { expect, test } from '@jupyterlab/galata';
 test('Should add a Notebook Jobs card to JupyterLab launcher', async ({
   page
 }) => {
-  await page.getByRole('tabpanel', { name: 'Launcher' }).waitFor({ state: 'detached' });
-  expect(page.getByTitle('Notebook Jobs')).toBeDefined();
+  await page.getByRole('tabpanel', { name: 'Launcher' });
+  expect(page.getByTitle('Notebook Job')).toBeDefined(); 
 });
