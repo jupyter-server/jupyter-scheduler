c7494bfce33a4bd9edc9e0297f9a82e402b53bf4
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-07-18 15:18:49 -0700

    define harcoded snapshot names at top level in all caps

--- ui-tests/tests/jupyter_scheduler.spec.ts
+++ ui-tests/tests/jupyter_scheduler.spec.ts
@@ -1,6 +1,13 @@
 import { expect, test } from '@jupyterlab/galata';
 import { SchedulerHelper } from '../helpers/SchedulerHelper';
 
+const CREATE_FROM_NOTEBOOK_SNAPSHOT_FILENAME = 'create-view-from-toolbar.png';
+const CREATE_VIEW_SNAPSHOT_FILENAME = 'create-view-empty.png';
+const LAUNCHER_SNAPSHOT_FILENAME = 'launcher-with-scheduler.png';
+const LIST_VIEW_IN_PROGRESS_SNAPSHOT_FILENAME = 'list-view-in-progress.png';
+const NOTEBOOK_SNAPSHOT_FILENAME= 'notebook-with-createjob-button.png';
+const RIGHTCLICK_MENU_SNAPSHOT_FILENAME = 'filebrowser-notebook-rightclick-menu.png';
+
 /**
  * Don't load JupyterLab webpage before running the tests.
  * This is required to ensure we capture all log messages.
@@ -23,10 +30,9 @@ test.describe('Jupyter Scheduler integration tests for JupyterLab', () => {
     const launcher = page.locator('div[role="main"] >> text=Launcher');
     await launcher.waitFor();
     const launcherCard = schedulerHelper.launcherCardLocator;
-    const launcherSnapshot = 'launcher-with-scheduler.png';
 
     await expect(launcherCard).toBeVisible();
-    expect(await page.screenshot()).toMatchSnapshot(launcherSnapshot);
+    expect(await page.screenshot()).toMatchSnapshot(LAUNCHER_SNAPSHOT_FILENAME);
   });
 
   test('"Create a notebook job" button is visible in notebook toolbar and leads to "Create a Job" page', async ({
@@ -40,11 +46,9 @@ test.describe('Jupyter Scheduler integration tests for JupyterLab', () => {
       .locator('.jp-Notebook-ExecutionIndicator[data-status="idle"]')
       .waitFor();
     const createJobButton = schedulerHelper.notebookToolbarButtonLocator;
-    const notebookSnapshot = 'notebook-with-createjob-button.png';
-    const createViewSnapshot = 'create-view-from-toolbar.png';
 
     await expect(createJobButton).toBeVisible();
-    expect(await page.screenshot()).toMatchSnapshot(notebookSnapshot);
+    expect(await page.screenshot()).toMatchSnapshot(NOTEBOOK_SNAPSHOT_FILENAME);
     await page.menu.clickMenuItem('File>Save Notebook');
     await page.click('button:has-text("Rename")');
     await createJobButton.click();
@@ -52,7 +56,7 @@ test.describe('Jupyter Scheduler integration tests for JupyterLab', () => {
     await page.waitForSelector('text=Loading …', { state: 'hidden' });
 
     await page.waitForSelector('text=Saving Completed', { state: 'hidden' });
-    expect(await page.screenshot()).toMatchSnapshot(createViewSnapshot);
+    expect(await page.screenshot()).toMatchSnapshot(CREATE_FROM_NOTEBOOK_SNAPSHOT_FILENAME);
   });
 
   test('"Create Notebook Job" item is visible when right clicking a notebook in File Browser and leads to "Create a Job" page', async ({
@@ -68,19 +72,17 @@ test.describe('Jupyter Scheduler integration tests for JupyterLab', () => {
     expect(await page.menu.isAnyOpen()).toBe(true);
     const righClickMenu = page.locator('ul.lm-Menu-content[role="menu"]');
     const createJobItem = schedulerHelper.filebrowserMenuItemLocator;
-    const righClickMenuSnapshot = 'filebrowser-notebook-rightclick-menu.png';
     await expect(createJobItem).toBeVisible();
     expect(await righClickMenu.screenshot()).toMatchSnapshot(
-      righClickMenuSnapshot
+      RIGHTCLICK_MENU_SNAPSHOT_FILENAME
     );
 
     await createJobItem.click();
     await page.waitForSelector('text=Loading …', { state: 'hidden' });
-    const createViewSnapshot = 'create-view-empty.png';
     await page.sidebar.close(
       (await page.sidebar.getTabPosition('filebrowser')) ?? undefined
     );
-    expect(await page.screenshot()).toMatchSnapshot(createViewSnapshot);
+    expect(await page.screenshot()).toMatchSnapshot(CREATE_VIEW_SNAPSHOT_FILENAME);
   });
 
   test('Create a job and see it in the list of jobs', async ({ page }) => {
@@ -101,9 +103,8 @@ test.describe('Jupyter Scheduler integration tests for JupyterLab', () => {
       (await page.sidebar.getTabPosition('filebrowser')) ?? undefined
     );
     const timeStamp = schedulerHelper.timestampLocator;
-    const listViewSnapshot = 'list-view-in-progress.png';
     const contentPanel = page.locator('#jp-main-content-panel');
-    await expect(contentPanel).toHaveScreenshot(listViewSnapshot, {
+    await expect(contentPanel).toHaveScreenshot(LIST_VIEW_IN_PROGRESS_SNAPSHOT_FILENAME, {
       mask: [timeStamp],
       maskColor: 'white',
       maxDiffPixelRatio: 0.01
