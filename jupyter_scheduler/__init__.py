d423ede7a751dae2326f90095ddb4e444346d61a
david qiu <david@qiu.dev>
2023-07-26 08:34:08 -0700

    Upgrade to JupyterLab 4 (#402)
    * remove unused dependency
    
    * upgrade dependencies to JL4
    
    * fix type errors
    
    * add dev-install script
    
    * update yarn.lock
    
    * bump JL version in build and workflows
    
    * pre-commit
    
    * fix lint
    
    * fix yarn.lock checksums
    
    * bump ts-jest to v29
    
    * upgrade to jest 29, typescript 4.3.0
    
    * pull in required JupyterLab 4 test configuration
    
    * upgrade to React 18 to be compatible w/ JL4
    
    * fix Build/test-isolated job
    
    * bump @jupyterlab/galata to latest, regenerate yarn.lock
    
    * try not deleting node
    
    * hardcode labextension name to fix Build/test_isolated job
    
    * call playwright directly via node to fix Build/integration-tests
    
    * use new JL4 galata config
    
    * fix and simplify E2E tests
    
    * remove outdated snapshots
    
    * add test:update script
    
    * add workflow_dispatch trigger to choose workflow branch
    
    * use jlpm when updating E2E snapshots
    
    * Update Playwright Snapshots
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- jupyter_scheduler/__init__.py
+++ jupyter_scheduler/__init__.py
@@ -1,19 +1,10 @@
 """Scheduling API for JupyterLab"""
-import json
-from pathlib import Path
-
 from ._version import __version__
 from .extension import SchedulerApp
 
-HERE = Path(__file__).parent.resolve()
-
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
 
 def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": data["name"]}]
+    return [{"src": "labextension", "dest": "@jupyterlab/scheduler"}]
 
 
 def _jupyter_server_extension_points():
