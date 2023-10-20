011a315c8fd0a9821c7e474d99e4d26cf2a4f39c
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-10-20 11:17:21 -0700

    add NotificationsPicker component

--- src/components/notifications-picker.tsx
+++ src/components/notifications-picker.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { InputLabel } from '@mui/material';
+import { FormControl, InputLabel, MenuItem } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { Stack } from './stack';
 
 export type NotificationsPickerProps = {
   notificationEvents: string[];
+  id: string;
 };
 
 export function NotificationsPicker(
   props: NotificationsPickerProps
 ): JSX.Element | null {
-  const { notificationEvents } = props;
-
-  if (!notificationEvents.length) {
+  if (!props.notificationEvents.length) {
     return null;
   }
+  const [selectedEvent, setSelectedEvent] = useState<string>('');
+  const { notificationEvents, id } = props;
+  const labelId = `${id}-label`;
+  const label = 'Notification Events';
+
+  const handleSelectChange = (event: SelectChangeEvent) => {
+    setSelectedEvent(event.target.value as string);
+  };
+
   return (
     <Stack size={2}>
       <InputLabel>{'Notifications'}</InputLabel>
+
+      <FormControl>
+        <InputLabel id={labelId}>{label}</InputLabel>
+        <Select
+          labelId={labelId}
+          id={id}
+          value={selectedEvent}
+          label={label}
+          onChange={handleSelectChange}
+        >
+          {notificationEvents.map(event => (
+            <MenuItem key={event} value={event}>
+              {event}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </Stack>
   );
 }
