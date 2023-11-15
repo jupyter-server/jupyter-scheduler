40815f9c133c3e48ea0c7551a80f5548442f8b05
Jason Weill <jweill@amazon.com>
2023-11-14 17:18:46 -0800

    Removes unneeded logic

--- jupyter_scheduler/pydantic_v1/__init__.py
+++ jupyter_scheduler/pydantic_v1/__init__.py
@@ -6,9 +6,3 @@ try:
     from pydantic.v1 import *
 except ImportError:
     from pydantic import *
-
-
-try:
-    _PYDANTIC_MAJOR_VERSION: int = int(metadata.version("pydantic").split(".")[0])
-except metadata.PackageNotFoundError:
-    _PYDANTIC_MAJOR_VERSION = 0
