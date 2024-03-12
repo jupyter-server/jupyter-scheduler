a4564bfc8ec27a5efb479bcc9a95cda1f000af56
Sanjiv Das <srdas@scu.edu>
2024-03-12 12:41:58 -0700

    Changed column header to "Input file" in Notebook Job Definitions (#496)
    * Changed column header to Input file in Notebook Job Definitions
    
    * Update list-jobs.tsx for column header change in Notebook Jobs table
    
    Changed the column headers for the inout file in both Notebook Jobs and Notebook Job Definitions to 'Input filename" - we choose “filename” over “file” is because we don’t show the absolute or relative path, just the name of the file in any directory.
    
    * Updated E2E snapshots for test

--- src/mainviews/list-jobs.tsx
+++ src/mainviews/list-jobs.tsx
@@ -119,7 +119,7 @@ export function ListJobsTable(props: IListJobsTableProps): JSX.Element {
     },
     {
       sortField: 'input_filename',
-      name: trans.__('Input file')
+      name: trans.__('Input filename')
     },
     {
       sortField: null,
