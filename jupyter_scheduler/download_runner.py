381dcae83ae5f23ea4fefd3cc139bc5cf13c6e10
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2024-04-10 14:37:07 -0700

    add traitlets-configurable downloads_poll_interval

--- jupyter_scheduler/download_runner.py
+++ jupyter_scheduler/download_runner.py
@@ -1,24 +1,44 @@
 import asyncio
+import traitlets
+from traitlets.config import LoggingConfigurable
 
+from jupyter_server.transutils import _i18n
 from jupyter_scheduler.download_manager import DownloadManager
 from jupyter_scheduler.job_files_manager import JobFilesManager
 
 
-class BaseDownloadRunner:
+class BaseDownloadRunner(LoggingConfigurable):
+    """Base download runner, this class's start method is called
+    at the start of jupyter server, and is responsible for
+    polling for downloads to download.
+    """
+
+    def __init__(self, config=None, **kwargs):
+        super().__init__(config=config)
+
+    downloads_poll_interval = traitlets.Integer(
+        default_value=3,
+        config=True,
+        help=_i18n(
+            "The interval in seconds that the download runner polls for downloads to download."
+        ),
+    )
+
     def start(self):
         raise NotImplementedError("Must be implemented by subclass")
 
 
 class DownloadRunner(BaseDownloadRunner):
+    """Default download runner that maintains a record and a queue of initiated downloads , and polls the queue every `poll_interval` seconds
+    for downloads to download.
+    """
+
     def __init__(
-        self,
-        download_manager: DownloadManager,
-        job_files_manager: JobFilesManager,
-        poll_interval: int = 5,
+        self, download_manager: DownloadManager, job_files_manager: JobFilesManager, config=None
     ):
+        super().__init__(config=config)
         self.download_manager = download_manager
         self.job_files_manager = job_files_manager
-        self.poll_interval = poll_interval
 
     async def process_download_queue(self):
         while not self.download_manager.queue.empty():
@@ -33,4 +53,4 @@ class DownloadRunner(BaseDownloadRunner):
         self.download_manager.populate_queue()
         while True:
             await self.process_download_queue()
-            await asyncio.sleep(self.poll_interval)
+            await asyncio.sleep(self.downloads_poll_interval)
