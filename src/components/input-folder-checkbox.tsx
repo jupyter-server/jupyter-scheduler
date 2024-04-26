abc5085d6a2589f7c86759976306017933cf64c5
Andrii Ieroshenko <aieroshe@amazon.com>
2024-04-26 10:21:31 -0700

    Update src/components/input-folder-checkbox.tsx
    Co-authored-by: Jason Weill <93281816+JasonWeill@users.noreply.github.com>

--- src/components/input-folder-checkbox.tsx
+++ src/components/input-folder-checkbox.tsx
@@ -25,7 +25,7 @@ export function PackageInputFolderControl(props: {
     );
   } else {
     helperText = trans.__(
-      "The scheduled job will have access to all files under input file's folder"
+      "The scheduled job will have access to all files under the input file's folder"
     );
   }
 
