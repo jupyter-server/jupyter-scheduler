ae879216e1c1df4bfb37df3e8de6de1032722562
asmita-sh <14326812+asmita-sharma1625@users.noreply.github.com>
2025-03-19 14:02:07 +0530

    Updated UI tests to match job creation UI from file browser and toolbar to the same snapshot

--- ui-tests/tests/jupyter_scheduler.spec.ts
+++ ui-tests/tests/jupyter_scheduler.spec.ts
@@ -1,14 +1,15 @@
 import { expect, test } from '@jupyterlab/galata';
 import { SchedulerHelper } from '../helpers/SchedulerHelper';
 
+/**
+ * Snapshot files are mapped by Galata basis the configuration set in file:
+ * https://github.com/jupyterlab/jupyterlab/blob/main/galata/update_snapshots.py
+ */
 enum FILENAMES {
   LAUNCHER = 'launcher.png',
   NOTEBOOK_TOOLBAR = 'notebook-toolbar.png',
   FILEBROWSER_MENU = 'filebrowser-menu.png',
-  // TODO: resolve this inconsistency in our frontend code. One entry point
-  // includes the file extension in the job name, the other does not.
-  CREATE_VIEW_FROM_TOOLBAR = 'create-view-from-toolbar.png',
-  CREATE_VIEW_FROM_FILEBROWSER = 'create-view-from-filebrowser.png',
+  CREATE_JOB_VIEW = 'create-job-view.png',
   LIST_VIEW = 'list-view.png'
 }
 
@@ -49,7 +50,7 @@ test.describe('Jupyter Scheduler', () => {
     await scheduler.createJobTbutton.click();
     await page.waitForSelector('text=Loading …', { state: 'hidden' });
     await page.waitForSelector('text=Saving Completed', { state: 'hidden' });
-    await scheduler.assertSnapshot(FILENAMES.CREATE_VIEW_FROM_TOOLBAR);
+    await scheduler.assertSnapshot(FILENAMES.CREATE_JOB_VIEW);
   });
 
   test('shows filebrowser menu item', async ({ page }) => {
@@ -66,7 +67,7 @@ test.describe('Jupyter Scheduler', () => {
   test('opens create job view from filebrowser menu item', async () => {
     await scheduler.createNotebook();
     await scheduler.openCreateJobFromFilebrowser();
-    await scheduler.assertSnapshot(FILENAMES.CREATE_VIEW_FROM_FILEBROWSER);
+    await scheduler.assertSnapshot(FILENAMES.CREATE_JOB_VIEW);
   });
 
   test('shows newly created job in job list view', async ({ page }) => {
